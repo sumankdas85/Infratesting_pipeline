15c2666a062e9521ba41705614d2358a404c22e7
unknown <sumankdas85@gmail.com>
2025-08-04 23:26:38 +0530

    Adding new rg

--- provider.tf
+++ provider.tf
@@ -17,6 +17,6 @@ backend "azurerm" {
 
 provider "azurerm" {
   features {}
-  subscription_id = "ae3dc4b8-c6c2-443e-b765-214338d62948"
+  subscription_id = "c316f505-7597-4175-b5db-d2949009d506"
 
 }
